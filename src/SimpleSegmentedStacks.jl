c6a36e4fd74b976155cd59ffe8ce6f3d7e1b7ec4
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-10 16:11:33 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/SimpleSegmentedStacks.jl
@@ -0,0 +1,11 @@
+baremodule SimpleSegmentedStacks
+
+module Internal
+
+using ..SimpleSegmentedStacks: SimpleSegmentedStacks
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule SimpleSegmentedStacks
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SimpleSegmentedStacks
+using Test
+
+@testset "SimpleSegmentedStacks.jl" begin
+    # Write your tests here.
+end
